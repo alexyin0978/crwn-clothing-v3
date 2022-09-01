18a183257870c1b64ba81a510ae30ad484b6d424
alex <yinchihkai0909@gmail.com>
2022-09-01 15:56:47 +0800

    firebase auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 //hooks
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 //containers
 import Home from "./containers/home/Home.container";
-import Auth from './containers/auth/Auth.container';
+import Auth from "./containers/auth/Auth.container";
 
 //high order component
-import Navbar from './hoc/navbar/Navbar.hoc';
-
+import Navbar from "./hoc/navbar/Navbar.hoc";
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Navbar />}>
+      <Route path="/" element={<Navbar />}>
         <Route index element={<Home />} />
         <Route path="auth" element={<Auth />} />
       </Route>
